b9ac559ea1e8e1249f32302f68f484f7bff52276
Arthur Cosentino <arthurcose@gmail.com>
2025-12-16 09:22:29 -0500

    Add kana to MICRO font

--- scripts/generate-kana-anim.js
+++ scripts/generate-kana-anim.js
@@ -51,20 +51,56 @@ const fonts = [
     xStart: 1,
     xStride: 8,
     yStride: 12,
-    frameDetails: "w=7 h=12 originy=1",
+    frameAttributes: "w=7 h=12 originy=1",
   },
   {
     name: "THIN",
     xStart: 1,
     xStride: 8,
     yStride: 13,
-    frameDetails: "w=7 h=13 originy=1",
+    frameAttributes: "w=7 h=13 originy=1",
+  },
+  {
+    name: "MICRO",
+    xStart: 1,
+    xStride: 8,
+    yStride: 6,
+    frameAttributes: "w=5 h=6 originy=1",
+    frameAttributeMap: [
+      // hiragana
+      ["w=6 h=6 originy=1", "ふかみれ"],
+      [
+        "w=7 h=6 originy=1",
+        "がぎぐげご ざじずぜぞ だぢづでど ばびぶべぼ ぱぴぷぺぽ",
+      ],
+      // katakana
+      ["w=6 h=6 originy=1", "ルヴ"],
+      [
+        "w=7 h=6 originy=1",
+        "ガギグゲゴ ザジズゼゾ ダヂヅデド バビブベボ パピプペポ",
+      ],
+      // katakana-small
+      ["w=3 h=6 originy=1", "ァィェ"],
+      ["w=4 h=6 originy=1", "ゥォュョ"],
+    ],
   },
 ];
 
 for (const font of fonts) {
-  const { xStart, xStride, yStride, frameDetails } = font;
+  const { xStart, xStride, yStride } = font;
 
+  // invert frame attribute map for faster lookup
+  const frameAttributeMap = {};
+
+  if (font.frameAttributeMap) {
+    for (const [attributes, graphemes] of font.frameAttributeMap) {
+      for (const grapheme of graphemes) {
+        frameAttributeMap[grapheme] = attributes;
+      }
+    }
+  }
+
+  // create sheets
   for (const sheetSuffix in sheetLayouts) {
     const sheetLayout = sheetLayouts[sheetSuffix];
 
@@ -74,9 +110,11 @@ for (const font of fonts) {
           const codePoint = grapheme.codePointAt(0);
           const codePointHex = codePoint.toString(16).toUpperCase();
 
+          const attrs = frameAttributeMap[grapheme] ?? font.frameAttributes;
+
           return [
             `animation state="${font.name}_U+${codePointHex}"`,
-            `frame x=${xStart + x * xStride} y=${y * yStride} ${frameDetails}`,
+            `frame x=${xStart + x * xStride} y=${y * yStride} ${attrs}`,
           ].join("\n");
         })
       )
